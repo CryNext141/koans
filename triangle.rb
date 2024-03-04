f819a58e5efa1b805c9203a63472ca50da4d5cb0
cryalex141 <tarasfurtak305@gmail.com>
2024-03-04 11:50:12 +0200

    #tests

--- triangle.rb
+++ triangle.rb
@@ -14,10 +14,6 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a <= 0 || b <= 0 || c <= 0 || (a + b <= c) || (b + c <= a) || (a + c <= b)
-    raise TriangleError, "Invalid triangle sides"
-  end
-
   if a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
